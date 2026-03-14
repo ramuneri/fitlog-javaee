f6997a28e17f0355d53ca51d9cca353f3eb7cc25
ramuneri <ramune.riau@gmail.com>
2026-03-14 21:44:37 +0200

    workoutPlans for creation and listing with JSF and JPA

---
+++ fitlog-javaee/src/main/java/lt/vu/fitlog/usecases/WorkoutPlans.java
@@ -0,0 +1,46 @@
+package lt.vu.fitlog.usecases;
+
+import lt.vu.fitlog.entities.WorkoutPlan;
+import lt.vu.fitlog.persistence.WorkoutPlanDAO;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Named
+@RequestScoped
+public class WorkoutPlans {
+
+    @Inject
+    private WorkoutPlanDAO workoutPlanDAO;
+
+    private List<WorkoutPlan> allWorkoutPlans;
+
+    private WorkoutPlan workoutPlanToCreate = new WorkoutPlan();
+
+    @PostConstruct
+    public void init() {
+        allWorkoutPlans = workoutPlanDAO.loadAll();
+    }
+
+    @Transactional
+    public String createWorkoutPlan() {
+        workoutPlanDAO.persist(workoutPlanToCreate);
+        return "index?faces-redirect=true";
+    }
+
+    public List<WorkoutPlan> getAllWorkoutPlans() {
+        return allWorkoutPlans;
+    }
+
+    public WorkoutPlan getWorkoutPlanToCreate() {
+        return workoutPlanToCreate;
+    }
+
+    public void setWorkoutPlanToCreate(WorkoutPlan workoutPlanToCreate) {
+        this.workoutPlanToCreate = workoutPlanToCreate;
+    }
+}

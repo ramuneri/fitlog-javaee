e1d03953bb0d010194943434cc0f603268cfe764
ramuneri <ramune.riau@gmail.com>
2026-04-15 09:20:12 +0300

    @Specializes for WorkoutPlans.java usecase

---
+++ fitlog-javaee/src/main/java/lt/vu/fitlog/usecases/SpecializedWorkoutPlans.java
@@ -0,0 +1,15 @@
+package lt.vu.fitlog.usecases;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Specializes;
+
+@RequestScoped
+@Specializes
+public class SpecializedWorkoutPlans extends WorkoutPlans {
+
+    @Override
+    public String createWorkoutPlan() {
+        System.out.println("USING SPECIALIZED WorkoutPlans");
+        return super.createWorkoutPlan();
+    }
+}

9d73bd0fcdf27d98221b92c6e303c833c2dd92a0
ramuneri <ramune.riau@gmail.com>
2026-04-19 17:57:36 +0300

    @Decorator in workoutPlan

---
+++ fitlog-javaee/src/main/java/lt/vu/fitlog/decorators/WorkoutPlanDAODecorator.java
@@ -0,0 +1,46 @@
+package lt.vu.fitlog.decorators;
+
+import lt.vu.fitlog.entities.Exercise;
+import lt.vu.fitlog.entities.WorkoutPlan;
+import lt.vu.fitlog.persistence.WorkoutPlanDAO;
+
+import javax.decorator.Decorator;
+import javax.decorator.Delegate;
+import javax.inject.Inject;
+import java.util.List;
+
+@Decorator
+public abstract class WorkoutPlanDAODecorator implements WorkoutPlanDAO {
+
+    @Inject
+    @Delegate
+    private WorkoutPlanDAO workoutPlanDAO;
+
+    @Override
+    public void persist(WorkoutPlan workoutPlan) {
+        System.out.println("DECORATOR: before persisting workout plan: " + workoutPlan.getName());
+        workoutPlanDAO.persist(workoutPlan);
+        System.out.println("DECORATOR: after persisting workout plan");
+    }
+
+    @Override
+    public List<WorkoutPlan> loadAll() {
+        System.out.println("DECORATOR: loading all workout plans yappy");
+        return workoutPlanDAO.loadAll();
+    }
+
+    @Override
+    public WorkoutPlan findOne(Long id) {
+        return workoutPlanDAO.findOne(id);
+    }
+
+    @Override
+    public List<Exercise> findExercisesByWorkoutPlanId(Long workoutPlanId) {
+        return workoutPlanDAO.findExercisesByWorkoutPlanId(workoutPlanId);
+    }
+
+    @Override
+    public WorkoutPlan update(WorkoutPlan workoutPlan) {
+        return workoutPlanDAO.update(workoutPlan);
+    }
+}
